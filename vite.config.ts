689c38125da8396bcbe22538b1df0222cce441d4
Takahito Nakano <kano@ponko2.jp>
2026-03-18 00:00:00 +0900

    chore(config): Sort keys in config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
   staged: {
     "*": "vp check --fix",
   },
-  lint: { options: { typeAware: true, typeCheck: true } },
 });
