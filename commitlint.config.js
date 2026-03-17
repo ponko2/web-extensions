ba881956d11744fb65e9c96964c24a52bccb7fe4
Takahito Nakano <kano@ponko2.jp>
2026-03-18 00:00:00 +0900

    ci: Set up commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // ProTip: Great commit summaries are 50 characters or less. Place extra information in the extended description.
+    "header-max-length": [2, "always", 50],
+    // subjectでの日本語使用を許可
+    "subject-case": [0],
+  },
+  ignores: [
+    (commit) => /^Signed-off-by: dependabot\[bot] <support@github\.com>$/m.test(commit),
+    (commit) =>
+      /^Signed-off-by: renovate\[bot] <29139614\+renovate\[bot]@users\.noreply\.github\.com>$/m.test(
+        commit,
+      ),
+  ],
+};
