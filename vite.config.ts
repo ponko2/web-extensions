c42c248a89ae43a5eabaf46b053fea64fc9a8c87
Takahito Nakano <kano@ponko2.jp>
2026-03-18 00:00:00 +0900

    chore(config): Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  fmt: {
+    sortImports: {},
+  },
   lint: {
     options: {
+      denyWarnings: true,
+      reportUnusedDisableDirectives: "error",
       typeAware: true,
       typeCheck: true,
     },
