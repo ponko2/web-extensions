e2a91ec5f55329acc2e71246a55d066aaca715d1
Takahito Nakano <kano@ponko2.jp>
2026-04-16 23:23:38 +0900

    refactor: rename to and-udm-14

--- packages/and-udm-14/wxt.config.ts
+++ packages/and-udm-14/wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: "Refined Google",
+    name: "&udm=14",
     action: {},
     host_permissions: ["*://www.google.com/search"],
     permissions: ["declarativeNetRequest", "declarativeNetRequestWithHostAccess", "storage"],
